5f2f62dc2d392507042a395ceb11ab7a1752328a
morganpstanley <morganpstanley@gmail.com>
2019-11-02 02:51:54 -0600

    add avg_rating method to movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,14 @@ class Movie < ApplicationRecord
 
     validates :name, :director, :genre, presence: true
 
+    def avg_rating
+        movies = UserMovie.select{ |m| m.movie_id == self.id}
+        rating_count = 0.0
+        movies.each do |m|
+         rating_count += m.rating
+        end
+        (rating_count / movies.count).round(2)
+     end
+ 
+
 end
