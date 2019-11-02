fb333e39aaeeee770c2f5b7b221c83df936ef9a5
morganpstanley <morganpstanley@gmail.com>
2019-11-01 23:38:16 -0600

    add private user params and require login method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,18 @@ class UsersController < ApplicationController
     end
 
     def create
+        binding.pry
+        @user = User.new(user_params)
+    end
+
+private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation, :email)
+    end
+
+    def require_login
+        return head(:forbidden) unless session.include? :user_id
     end
 
 end
