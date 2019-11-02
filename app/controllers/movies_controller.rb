764f69c012f061ef7690b492c375a05d4dee13db
morganpstanley <morganpstanley@gmail.com>
2019-11-01 23:27:01 -0600

    define show index new create for controllers

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,18 @@
 class MoviesController < ApplicationController
+
+    def show
+    end
+    
+    def index
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        binding.pry
+        @user = User.new(user_params)
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+    def show
+    end
+    
+    def index
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+    end
+
 end
