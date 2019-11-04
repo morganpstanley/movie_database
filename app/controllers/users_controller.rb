84d55ac2343cb692818a0dbed3e62e776732c878
morganpstanley <morganpstanley@gmail.com>
2019-11-04 15:36:56 -0700

    add session after user create action and add current_user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params) 
         if @user.save
+            session[:user_ud] = @user.id
             redirect_to user_path(@user)
         else
             render :new
