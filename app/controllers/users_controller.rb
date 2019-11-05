f5feec433f4242e2e6583a1f538a19e161954dd9
morganpstanley <morganpstanley@gmail.com>
2019-11-04 22:17:28 -0700

    misc cleaning and reformatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
     def show
-        @user = User.find_by(username: params[:username])
+        @user = current_user
     end
     
     def index
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params) 
         if params[:user][:password] == params[:user][:password_confirmation] && @user.save
-            session[:user_ud] = @user.id
+            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             render :new
@@ -34,6 +34,11 @@ class UsersController < ApplicationController
         else
             redirect_to :login
         end
+    end
+
+    def logout
+        session.clear
+        redirect_to login_path
     end
 
 private
