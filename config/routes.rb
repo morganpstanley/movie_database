02371f90a3915c2ee94dc56eab0da09cf96af295
morganpstanley <morganpstanley@gmail.com>
2019-11-05 02:30:52 -0700

    add logout route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   
   get '/login', to: 'users#signin'
   post '/login', to: 'users#login'
+  get '/logout', to: 'users#logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
