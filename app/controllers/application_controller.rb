1e1f615748f420341085e3656c13cb123ef28420
morganpstanley <morganpstanley@gmail.com>
2019-11-05 02:30:15 -0700

    add find_user and apply it and current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
     def current_user
         User.find_by(id: session[:user_id])
     end
+
+    def find_user
+        @user = User.find_by(id: params[:id])
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
     def show
-        @user = current_user
+        find_user
     end
     
     def index
