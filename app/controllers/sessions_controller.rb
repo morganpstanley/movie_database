be443e6d47c5e11938493e61d80ef9e94ae9ada0
morganpstanley <morganpstanley@gmail.com>
2019-11-12 03:41:56 -0700

    add login route for real. Aaaargh

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,16 @@ class SessionsController < ApplicationController
         user = User.new
     end
 
+    def login
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+             redirect_to user_path(@user)
+        else
+            redirect_to :login
+        end
+    end
+
     def create
         if auth
             user = User.find_or_create_by_omniauth(auth)
