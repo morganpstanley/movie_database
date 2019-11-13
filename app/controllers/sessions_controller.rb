d2213f029ad83fa05a7ac300415e94e067c4e407
morganpstanley <morganpstanley@gmail.com>
2019-11-13 02:55:57 -0700

    fixed login params error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
     end
 
     def login
-        @user = User.find_by(username: params[:user][:username])
-        if @user && @user.authenticate(params[:user][:password])
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
              redirect_to user_path(@user)
         else
