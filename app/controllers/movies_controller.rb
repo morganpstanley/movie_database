91c2244c902e9b4e5c09b5ee3d89ab3f5babcb45
morganpstanley <morganpstanley@gmail.com>
2019-11-13 21:22:26 -0700

    remove create and new routes from movies, because I moved them to the admin/movies controller, but for some reason didn't remove them from here.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,24 +14,7 @@ class MoviesController < ApplicationController
         end
     end
 
-    def new
-        @movie = Movie.new
-    end
-
-    def create
-        @movie = Movie.new(movie_params)
-        if @movie.save
-            redirect_to movie_path(@movie)
-        else
-            render :new
-        end
-    end
-
 private
-
-    def movie_params
-        params.require(:movie).permit(:name, :director, :release_date, :genre)
-    end
 
     def require_login
         return head(:forbidden) unless session.include?(:user_id)
