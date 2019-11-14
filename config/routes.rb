2547807f6e30d5eae8fa61bfb628ce52e69a1ee4
morganpstanley <morganpstanley@gmail.com>
2019-11-13 23:31:19 -0700

    organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,25 @@
 Rails.application.routes.draw do
+
+  root 'sessions#signin'
+
+  #user routes
   resources :users do 
     resources :movies, only: [:index]
   end
+  post '/users/:id/movies', to: 'users#add_movie'
 
+  #movie routes
+  get '/movies/released_before', to: 'movies#released_before'
   resources :movies
 
+  #admin routes
   namespace :admin do
     resources :movies, only: [:new, :create, :edit, :update]
   end
   
-  root 'users#index'
+  #session routes
   get '/login', to: 'sessions#signin'
   post '/login', to: 'sessions#login'
   get '/logout', to: 'sessions#logout'
-  post '/users/:id/movies', to: 'users#add_movie'
   match '/auth/github/callback', to: 'sessions#create', via: [:get, :post]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
