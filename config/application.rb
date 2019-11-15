3c2999cd2fae026ef26e85fc7d3c2326ec455abf
morganpstanley <morganpstanley@gmail.com>
2019-11-14 17:32:25 -0700

    add force_ssl to application.rb because I am probably supposed to

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module MovieDatabase
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # the framework and any gems in your application
+    config.force_ssl = true
   end
 end
