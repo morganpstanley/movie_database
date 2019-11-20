98ec5b2c8ef57c096d942049e03ab6382d277581
morganpstanley <morganpstanley@gmail.com>
2019-11-20 04:07:17 -0700

    add helper method login_through_github and use in session/new and user/new views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
     def current_user
         User.find_by(id: session[:user_id])
     end
+
+    def login_through_github
+        button_to('Log in with Github', '/auth/github')
+    end
 end
