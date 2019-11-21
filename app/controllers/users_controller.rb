a718fe38155d4845d2620d3e25433eacf6b822bc
morganpstanley <morganpstanley@gmail.com>
2019-11-20 19:53:44 -0700

    add edit user route and links and move user new and edit views to _form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,19 @@ class UsersController < ApplicationController
         end  
     end
 
+    def edit
+        @user = User.find_by(id: params[:id])
+    end
+
+    def update
+        @user = User.find_by(id: params[:id])
+        if @user.update(user_params)
+            redirect_to user_path(@user)
+        else
+            render :edit
+        end
+    end
+
 private
 
     def user_params
